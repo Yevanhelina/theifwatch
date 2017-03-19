7af2f79673b304d7c3b2381b47c2b599ad9bb6b5
hunter_yeva <evang.dnepr@gmail.com>
2017-03-19 19:53:28 +0200

    adding Json about

--- src/main/java/com/metroteam/theifwatch/controllers/HomeController.java
+++ src/main/java/com/metroteam/theifwatch/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package com.metroteam.theifwatch.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -11,4 +15,13 @@ public class HomeController {
 	public String index () {
 		return "index";
 	}
+	@RequestMapping(value="/aboutjson",method = RequestMethod.GET)
+	public @ResponseBody Map aboutJson () {
+		Map about = new HashMap<String, String>();
+		about.put("teamname", "MetroTeam");
+		about.put("projectName", "TheifWatch");
+		about.put("Year", "2017");
+		return about;
+	}
+	
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+	<h2>Hello World!</h2>
+	<a href="${pageContext.request.contextPath}/aboutjson/">About us in Json</a>
 </body>
 </html>
